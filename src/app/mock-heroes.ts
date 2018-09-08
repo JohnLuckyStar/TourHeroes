99430634171f3ad27eba3c1d1154301c5c05fcc7
JohnLuckyStar <johnerincon@gmail.com>
2018-09-08 16:15:09 -0500

    Repositorio inicial del folder Tour que contiene la practica de Tour of Heroes de Angular.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Superman', superpower: 'FlyStrongerRaysX' },
+  { id: 12, name: 'Batman', superpower: 'VeryMoney' },
+  { id: 13, name: 'SpiderMan', superpower: 'FlyStrongerRaysX' },
+  { id: 14, name: 'Ironman', superpower: 'FlyStrongerRaysX' },
+  { id: 15, name: 'Hulk', superpower: 'Fuerza' },
+  { id: 16, name: 'Thor', superpower: 'FlyStrongerRaysX' },
+  { id: 17, name: 'Mujer Maravilla', superpower: 'FlyStrongerRaysX' },
+  { id: 18, name: 'America Captain', superpower: 'FlyStrongerRaysX' },
+  { id: 19, name: 'Wolverine', superpower: 'FlyStrongerRaysX' },
+  { id: 20, name: 'Deadpool', superpower: 'FlyStrongerRaysX' }
+];
